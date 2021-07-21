8e26ce506e642d5bdd87c2d4d697c1af90ebab26
JaquelineGTZ <87338448+JaquelineGTZ@users.noreply.github.com>
2021-07-21 14:28:52 -0600

    Add files via upload

---
+++ 10/js-demo/home.js
@@ -0,0 +1,43 @@
+/*
+const app = {
+    productId: 12345,
+    userName: 'Joe',
+    orderNumber: 789
+};
+
+function showProductId(){
+    console.log(app.productId);
+}
+
+showProductId();
+*/
+
+/*
+let productId = 12345;
+
+function showProductId(){
+
+    function fix() {
+        console.log('in fix ', productId);
+    }
+    fix();
+
+    console.log('in showProductId: ', productId);
+}
+
+showProductId();
+*/
+
+
+/*
+showProductId();
+function showProductId() {
+    console.log(123);
+}
+*/
+
+'use strict';
+
+const productId = 1234;
+
+console.log(productId);
