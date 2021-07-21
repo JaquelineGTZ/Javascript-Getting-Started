2542803ce2d33c2fc24c3d9f772dcf32cf9725a5
JaquelineGTZ <87338448+JaquelineGTZ@users.noreply.github.com>
2021-07-21 14:20:59 -0600

    Add files via upload

---
+++ 06/js-demo/home.js
@@ -0,0 +1,49 @@
+
+/*
+if (+(1.1 + 1.3).toFixed(2) === 2.4) {
+    let message = 'hello'
+    showMessage(message);
+}
+*/
+/*
+let price = 4;
+if (price > 10) {
+    showMessage('true');
+}
+else if (price < 5){
+    showMessage('less than 5');
+}
+*/
+/*
+if (1 === "1") {
+    showMessage('true');
+}
+else {
+    showMessage('false');
+}
+*/
+/*
+let price = 20;
+(price > 10) ? showMessage('yes') : showMessage('no');
+*/
+/*let price = 20;
+let message = (price < 10) ? 'yes' : 'no';
+showMessage(message) */
+
+/*for (let i = 0; i < 5; i++) {
+    console.log(i);
+}*/
+/*for (let i = 0; i < 5; i++) {
+    console.log(i);
+}*/
+
+/*let i = 4;
+while (i > 0) {
+    console.log(i);
+    i--;
+}*/
+let i = 4;
+do{
+    console.log(i);
+    i--;
+}while (i > 0);
