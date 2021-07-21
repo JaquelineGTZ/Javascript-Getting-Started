e6d4aa2475908627e634361350ec1dfdf4430ea4
JaquelineGTZ <87338448+JaquelineGTZ@users.noreply.github.com>
2021-07-21 14:28:23 -0600

    Add files via upload

---
+++ 09/js-demo/home.js
@@ -0,0 +1,40 @@
+/*
+const values = ['a', 'b', 'c'];
+
+console.log(Array.isArray(values));
+*/
+
+/*
+const values = ['a', 'b', 'c'];
+values[0] = 'aaa';
+console.log(values[0]);
+*/
+
+/*
+const values = ['a', 'b', 'c'];
+const first = values.shift();
+console.log(values, first);
+*/
+/*
+const values = ['a', 'b', 'c', 'd', 'e'];
+
+ values.splice(2, 1, 'hello');
+
+console.log(values);
+*/
+
+/*
+const values = ['a', 'b', 'c'];
+
+const set = values.filter(function(item) {
+    console.log(item);
+});
+*/
+
+const containers = 
+    document.getElementsByClassName('container');
+containers[0].classList.add('d-none');
+console.log(containers);
+
+
+
