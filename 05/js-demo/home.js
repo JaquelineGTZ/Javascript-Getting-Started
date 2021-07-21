f4177836476490256307234f4c27ca7ac30f1aa3
JaquelineGTZ <87338448+JaquelineGTZ@users.noreply.github.com>
2021-07-21 14:19:40 -0600

    Add files via upload

---
+++ 05/js-demo/home.js
@@ -0,0 +1,20 @@
+/*let price = 12;
+
+price = price / 3;
+
+showMessage( price);
+*/
+/*let price = 12,
+    taxRate = 0.07
+
+showMessage(price * taxRate);*/
+/*let price = 25;
+
+showMessage(price);
+console.log(price);*/
+
+let person = {
+    firstName: 'Jhon',
+    lastName: 'Adams'
+};
+showMessage(firstName.lastName);
