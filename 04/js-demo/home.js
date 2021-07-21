9785ceded71d55f6f62a2c542af4ec108598fd81
JaquelineGTZ <87338448+JaquelineGTZ@users.noreply.github.com>
2021-07-21 14:15:10 -0600

    Add files via upload

---
+++ 04/js-demo/home.js
@@ -0,0 +1,17 @@
+
+//let price;
+
+// lost of code ...
+
+//price = 59.99;
+
+//more code here....
+
+//price = 29.99;
+
+//showMessage(price);
+
+let price = 25;
+//var price = 25;
+showMessage(price);
+console.log(price);
